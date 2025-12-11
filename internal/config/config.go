3043c7ef79f24b470806d702cf2010dbc70e8a7e
b3b0p831 <dnv19191@icloud.com>
2025-12-11 07:00:45 -0800

    updated tests

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,9 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"regexp"
 	"slices"
-	"strconv"
 	"time"
 
 	"github.com/spf13/viper"
@@ -25,36 +22,10 @@ type Config struct {
 	Flogger            *log.Logger
 }
 
-var re regexp.Regexp = *regexp.MustCompile(`^\d+[smh]$`)
-
 func (cfg *Config) Equal(otherCFG Config) bool {
 
 	return cfg.TargetDir == otherCFG.TargetDir && cfg.SourceDir == otherCFG.SourceDir &&
 		cfg.MaxFill == otherCFG.MaxFill && cfg.SyncDelay == otherCFG.SyncDelay && slices.Equal(cfg.ApprovedExtensions, otherCFG.ApprovedExtensions)
-}
-
-func GetTimeInterval(interval string) (time.Duration, error) {
-	if !re.Match([]byte(interval)) {
-		return 0, fmt.Errorf("interval string does not match format (i.e 1s, 3m, 5h): %v", interval)
-	}
-
-	timeValStr, lastChar := interval[:len(interval)-1], interval[len(interval)-1]
-	timeVal, err := strconv.ParseInt(timeValStr, 10, 64)
-	if err != nil {
-		return 0, fmt.Errorf("util/util.go: unable to ParseInt(timevalStr)")
-	}
-
-	switch lastChar {
-	case 's':
-		return time.Duration(timeVal) * time.Second, nil
-	case 'm':
-		return time.Duration(timeVal) * time.Minute, nil
-	case 'h':
-		return time.Duration(timeVal) * time.Hour, nil
-	default:
-		return 0, fmt.Errorf("invalid time unit: %v", lastChar)
-	}
-
 }
 
 func Load() *Config {
--- testing/filo_test.go
+++ testing/filo_test.go
@@ -7,7 +7,6 @@ import (
 	"runtime"
 	"strings"
 	"testing"
-	"time"
 
 	"os/exec"
 
@@ -62,71 +61,6 @@ func init() {
 			check:   nil,
 			wantErr: true,
 		},
-	}
-}
-
-func TestGetTimeInterval(t *testing.T) {
-	test_data := map[string]time.Duration{
-		// Valid basics
-		"1s":  time.Second,
-		"10m": 10 * time.Minute,
-		"2h":  2 * time.Hour,
-
-		// Boundary numbers
-		"0s":                    0,
-		"999999999s":            999999999 * time.Second, // huge, may overflow
-		"0001s":                 time.Second,
-		"18446744073709551615s": 0, // should fail
-
-		// Weird suffixes
-		"1sec":     0,
-		"2minutes": 0,
-		"3hour":    0,
-		"5ss":      0,
-		"10hh":     0,
-
-		// Nonnumeric junk
-		"5s5":  0,
-		"12m3": 0,
-		"５s":   0,
-		"1💥s":  0,
-
-		// Whitespace / control chars
-		" 1s":      0,
-		"1s ":      0,
-		"1\tm":     0,
-		"1\nh":     0,
-		"1\u200Bs": 0,
-
-		// Signs and decimals
-		"-10s": 0,
-		"+5m":  0,
-		"1.5h": 0,
-
-		// Case traps
-		"1S":   0,
-		"1M":   0,
-		"1H":   0,
-		"10Ms": 0,
-
-		// Empty / random junk
-		"":    0,
-		" ":   0,
-		"abc": 0,
-		"💣":   0,
-		"123": 0,
-		"s":   0,
-	}
-
-	for timeStr, expectedTimeVal := range test_data {
-		currentTimeVal, err := config.GetTimeInterval(timeStr)
-		if err != nil && expectedTimeVal != 0 {
-			t.Error(err)
-		}
-
-		if currentTimeVal != expectedTimeVal {
-			t.Errorf("util.GetTimeInterval(%s) != %v\n", timeStr, expectedTimeVal)
-		}
 	}
 }
 
